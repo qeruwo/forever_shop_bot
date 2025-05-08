d921b226b7a29cad5aada8cf84a5309206877de5
qeruwo <qeruwork@mail.ru>
2025-05-08 15:39:09 +0300

    2st commit to start message

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+from help_files.config import token
+from help_files.main_wording import start_text
+
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="7532267161:AAFWGUrUhBdRFPEvoUGygBNd054PY9Q_R7c")
+bot = Bot(token=token)
 # Диспетчер
 dp = Dispatcher()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer(start_text)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
